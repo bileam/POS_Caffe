a52f3ad79bdcdc6e63ca2c05e1758d2b146fc16f
bileam mangalla <bileammangalla12@gmail.com>
2026-02-18 18:04:40 +0700

    fetching data transaksi to riwayat transaksi

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -29,11 +29,11 @@ const Sidebar = () => {
           <img src="" alt="" />
           <button
             onClick={() => setBurger((prev) => !prev)}
-            className={`bg-[#357c4d] rounded-full translate-x-9 p-2 shadow cursor-pointer focus:scale-95 active:scale-90  flex flex-col gap-2`}
+            className={`bg-[#357c4d] rounded-full ${
+              burger ? "rotate-0 delay-500" : "rotate-180 delay-500"
+            } translate-x-9 p-2 text-white shadow transition-all duration-700 ease-in-out cursor-pointer focus:scale-95 active:scale-90  flex flex-col gap-2`}
           >
-            <div className="w-5 outline outline-white "></div>
-            <div className="w-5 outline outline-white "></div>
-            <div className="w-5 outline outline-white "></div>
+            {"->"}
           </button>
         </div>
         <NavLink
@@ -54,7 +54,7 @@ const Sidebar = () => {
                    
                 ${
                   burger
-                    ? "opacity-0 -translate-x-15  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
+                    ? "opacity-0 -translate-x-10  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
                     : "opacity-100 translate-x-0 text-[#357c4d]  "
                 }
                 
@@ -82,7 +82,7 @@ const Sidebar = () => {
            
         ${
           burger
-            ? "opacity-0 -translate-x-15  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
+            ? "opacity-0 -translate-x-10  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
             : "opacity-100 translate-x-0 text-[#357c4d]  "
         }
         
@@ -110,7 +110,7 @@ const Sidebar = () => {
            
         ${
           burger
-            ? "opacity-0 -translate-x-15  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
+            ? "opacity-0 -translate-x-10  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
             : "opacity-100 translate-x-0 text-[#357c4d]  "
         }
         
@@ -138,7 +138,7 @@ const Sidebar = () => {
            
         ${
           burger
-            ? "opacity-0 -translate-x-15  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
+            ? "opacity-0 -translate-x-10  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
             : "opacity-100 translate-x-0 text-[#357c4d]  "
         }
         
@@ -166,7 +166,7 @@ const Sidebar = () => {
            
         ${
           burger
-            ? "opacity-0 -translate-x-15  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
+            ? "opacity-0 -translate-x-10  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
             : "opacity-100 translate-x-0 text-[#357c4d]  "
         }
         
@@ -188,17 +188,15 @@ const Sidebar = () => {
           <img src={icon5} alt="" className="z-10" />
           <div className="overflow-hidden ">
             <span
-              className={` 
-        inline-block whitespace-nowrap
-        transition-all duration-500 ease-in-out
-           
-        ${
-          burger
-            ? "opacity-0 -translate-x-15  text-[#357c4d] bg-[#d4e7dc]  group-hover:opacity-100 px-2 rounded-md  group-hover:left-12   top-2 group-hover:translate-x-0 absolute "
-            : "opacity-100 translate-x-0 text-[#357c4d]  "
-        }
-        
-      `}
+              className={`
+              inline-block whitespace-nowrap
+              transition-all duration-500 ease-in-out
+              ${
+                burger
+                  ? "opacity-0 -translate-x-10 text-[#357c4d] bg-[#d4e7dc] group-hover:opacity-100 px-2 rounded-md absolute left-12 top-2 group-hover:translate-x-0"
+                  : "opacity-100 translate-x-0 text-[#357c4d]"
+              }
+            `}
             >
               Report
             </span>
--- src/pages/RiwayatTransaksi.jsx
+++ src/pages/RiwayatTransaksi.jsx
@@ -3,6 +3,7 @@ import Title from "../components/Title";
 
 import { TransaksiContext } from "../Context/Transaksi";
 import { useContext } from "react";
+import Input from "../components/Input";
 const RiwayatTransaksi = () => {
   const { ListTransaksi } = useContext(TransaksiContext);
   // namaPemesan
@@ -12,14 +13,24 @@ const RiwayatTransaksi = () => {
   // jam
   console.log(ListTransaksi);
   return (
-    <div className="h-screen flex flex-col overflow-hidden gap-2">
+    <div className="h-screen flex flex-col overflow-hidden gap-2 pb-15">
       <Title>Riwayat transaksi</Title>
-      <div className="flex-1 bg-white  p-3 flex  gap-2 overflow-hidden">
-        <div className="overflow-y-auto min-w-100">
+      <div className="  w-[40%] flex gap-2">
+        <Input>Cari Transaksi</Input>
+        <input type="date" className="border rounded px-2 border-[#357c4d]" />
+      </div>
+      <div className="flex-1 bg-white  p-3 flex  gap-2 overflow-hidden rounded-md">
+        <div className="overflow-y-auto flex-1 space-y-2 relative">
+          {ListTransaksi.length === 0 && (
+            <div className="text-[#d4e7dc] text-center">
+              Belum ada transaksi
+            </div>
+          )}
+
           {[...ListTransaksi]
             .sort((a, b) => b.id - a.id)
             .map((item) => (
-              <button className="flex flex-col shadow-md hover:shadow-[#d4e7dc] p-2 rounded-md text-start w-100 cursor-pointer active:scale-95 transition-all duration-500 ease-in-out">
+              <button className="flex flex-col border border-[#357c4d]  shadow-md hover:shadow-[] p-2 rounded-md text-start w-full cursor-pointer active:scale-95 transition-all duration-500 ease-in-out">
                 <h1 className="font-bold">{item.namaPemesan}</h1>
                 <h1 className="text-[0.9rem]">Rp. {item.total}</h1>
                 <div className="flex justify-between text-[0.8rem]">
@@ -32,12 +43,12 @@ const RiwayatTransaksi = () => {
               </button>
             ))}
         </div>
-        <div className="shadow flex-1 px-2">
+        {/* <div className="shadow flex-1 px-2">
           <h1 className="inline-block relative ">
             Detail Pesanan
             <span className="inline-block absolute bottom-0 w-full border left-0 "></span>
           </h1>
-        </div>
+        </div> */}
       </div>
     </div>
   );
